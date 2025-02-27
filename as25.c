7ffc54879d859fa76306391530ade2a2e4ca0db0
dwitiyaroy <dwitiya28804@gmail.com>
2025-02-27 11:01:34 +0530

    Add files via upload

---
+++ as25.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter three numbers : ");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			printf("The greatest number is %d",a);
+        }
+        else
+       {
+ 	        printf("The greatest number is %d",c);
+       }
+	}
+	else
+   {
+   	    if(b>c)
+   	    {
+   	    	printf("The greatst number is %d",b);
+   	    }
+   	    else
+	    {
+	    	printf("The greatest number is %d",c);
+		}
+}
+   return 0;
+}
