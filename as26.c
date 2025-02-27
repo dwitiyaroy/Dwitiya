dbcf724d3a4f404c54c81b781fd1357474289331
dwitiyaroy <dwitiya28804@gmail.com>
2025-02-27 11:01:59 +0530

    Add files via upload

---
+++ as26.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("The size of integer is %u bytes\n",sizeof(int));
+	printf("The size of float is %u bytes\n",sizeof(float));
+	printf("The size of double is %u bytes\n",sizeof(double));
+	printf("The size of char is %u byte\n",sizeof(char));
+	return 0;	
+}
